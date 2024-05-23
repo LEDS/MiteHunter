b669cc21452086478e6e0a5bc8fc3df809dcffbe
Marcos <marcosviniciussouzadossantos19@gmail.com>
2024-05-23 11:04:13 -0300

    feature(MITE-0): Separei o modulo de configuração para posteriormente fazer um container

---
+++ infra/commands/python_config/configuration.py
@@ -0,0 +1,44 @@
+import mysql.connector
+import time
+import os
+
+DB_NAME = os.getenv('MYSQL_DATABASE')
+DB_USER = os.getenv('MYSQL_USER')
+DB_PASSWORD = os.getenv('MYSQL_PASSWORD')
+DB_HOST = os.getenv('MYSQL_HOST')
+DB_PORT = os.getenv('MYSQL_PORT')
+
+def try_connection():
+    connection_config = {
+        "host": os.getenv('MYSQL_HOST'),
+        "user": os.getenv('MYSQL_USER'),
+        "password": os.getenv('MYSQL_PASSWORD'),
+        "port": os.getenv('MYSQL_PORT')
+    }
+
+    for c in range(5):
+        try:
+            return mysql.connector.connect(**connection_config)
+        except mysql.connector.Error as e:
+            print(f"ERROR: Failed to connect to database: {e}")
+            print("Retrying in 1 minute...")
+            time.sleep(60)
+    return None
+
+def budibase_ip_catch():
+    connection = try_connection()
+    if connection is None:
+        return
+
+    with connection.cursor() as cursor:
+        cursor.execute("DELETE FROM MiteHunter.BudibaseIpCatch")
+        cursor.execute("INSERT INTO MiteHunter.BudibaseIpCatch (ip) VALUES (%s)", (os.getenv('MYSQL_HOST'),))
+        connection.commit()
+
+    connection.close()
+
+def main():
+    budibase_ip_catch()
+
+if __name__ == '__main__':
+    budibase_ip_catch()
