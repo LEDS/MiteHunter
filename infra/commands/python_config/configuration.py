190a2c0f6136de5dc69a42d8cfa41bdb8e1db599
OficialMark <marcosviniciussouzadossantos19@gmail.com>
2024-05-27 13:05:47 -0300

    refactor(MITE-0): Atualizando codigos

--- infra/commands/python_config/configuration.py
+++ infra/commands/python_config/configuration.py
@@ -1,14 +1,37 @@
 import mysql.connector
 import time
 import os
+import subprocess
+
+def execute_sql_dump():
+    # Comando que você deseja executar
+    comando = "cat backup.sql | docker exec -i mysql /usr/bin/mysql -u root --password=12345 MiteHunter"
+
+    # Executar o comando
+    resultado = subprocess.run(comando, shell=True, capture_output=True, text=True)
+
+    # Verificar se a execução foi bem-sucedida
+    if resultado.returncode == 0:
+        # Imprimir a saída do comando
+        print("Saída do comando:")
+        print(resultado.stdout)
+    else:
+        # Imprimir mensagem de erro se a execução falhou
+        print("Erro ao executar o comando.")
+
+
+def databaseDump():    
+    execute_sql_dump()
+
 
 def try_connection():
     connection_config = {
-        "host": os.getenv('MYSQL_HOST'),
-        "user": os.getenv('MYSQL_USER'),
-        "password": os.getenv('MYSQL_PASSWORD'),
-        "port": os.getenv('MYSQL_PORT')
+        "host": os.environ['MYSQL_HOST'],
+        "user": os.environ['MYSQL_USER'],
+        "password": os.environ['MYSQL_PASSWORD'],
+        "port": os.environ['MYSQL_PORT']
     }
+
 
     for c in range(5):
         try:
@@ -32,7 +55,9 @@ def budibase_ip_catch():
     connection.close()
 
 def main():
+    databaseDump()
     budibase_ip_catch()
+    databaseDump()
 
 if __name__ == '__main__':
     budibase_ip_catch()
